5998f31c6f6aadb7df043b0603dfa10aadf354a2
Fabien Göhl <fabien-goehl@web.de>
2018-07-04 12:12:10 +0200

    Added ImageToForm Method

--- Block_Jumps/Form1.cs
+++ Block_Jumps/Form1.cs
@@ -21,5 +21,40 @@ namespace Block_Jumps
         {
 
         }
-    }
+
+		private void ImageToLevel(string levelFile) {
+			//Einlesen des Bildes per File
+			Image img = Image.FromFile(levelFile);
+
+			//Kopie des Bildes zu Bitmap
+			Bitmap map = new Bitmap(img);
+
+			//Pixelarray
+			Color[,] pixels = new Color[map.Width, map.Height];
+
+			//Initialisieren des Pixelarrays
+			for (int y = 0; y < map.Height; y++) {
+				for (int x = 0; x < map.Width; x++) {
+					pixels[x, y] = map.GetPixel(x, y);
+				}
+			}
+
+			//Geht jeden Pixel in Pixels durch
+			for (int y = 0; y < map.Height; y++) {
+				for (int x = 0; x < map.Width; x++) {
+					if (pixels[x, y].A == 0 && pixels[x, y].R == 0 && pixels[x, y].G == 0 && pixels[x, y].B == 0) {
+						// TODO: Add code for Transparent Pixels 
+					} else if (pixels[x, y].A == 255 && pixels[x, y].R < 10 && pixels[x, y].G < 10 && pixels[x, y].B < 10) {
+						// TODO: Add code for Black Pixels 
+					} else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 0 && pixels[x, y].B == 255) {
+						// TODO: Add code for Blue Pixels 
+					} else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 255 && pixels[x, y].B == 0) {
+						// TODO: Add code for Green Pixels 
+					} else if (pixels[x, y].A == 255 && pixels[x, y].R == 255 && pixels[x, y].G == 255 && pixels[x, y].B == 0) {
+						// TODO: Add code for Yellow Pixels 
+					}
+				}
+			}
+		}
+	}
 }
