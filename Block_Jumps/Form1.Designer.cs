d65290fc1e5a7b4859722cb5f810c308411b3e87
Jannik <jannikkallies@web.de>
2018-07-11 13:03:42 +0200

    Hindernis check und boden check implementiert

--- Block_Jumps/Form1.Designer.cs
+++ Block_Jumps/Form1.Designer.cs
@@ -28,21 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.player = new System.Windows.Forms.PictureBox();
+            this.block = new System.Windows.Forms.PictureBox();
+            this.gameBorder = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).BeginInit();
+            this.gameBorder.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.player.Location = new System.Drawing.Point(268, 390);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(20, 20);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // block
+            // 
+            this.block.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.block.Location = new System.Drawing.Point(228, 448);
+            this.block.Name = "block";
+            this.block.Size = new System.Drawing.Size(100, 20);
+            this.block.TabIndex = 1;
+            this.block.TabStop = false;
+            // 
+            // gameBorder
+            // 
+            this.gameBorder.Controls.Add(this.player);
+            this.gameBorder.Controls.Add(this.block);
+            this.gameBorder.Location = new System.Drawing.Point(12, 81);
+            this.gameBorder.Name = "gameBorder";
+            this.gameBorder.Size = new System.Drawing.Size(560, 468);
+            this.gameBorder.TabIndex = 2;
             // 
             // Block_Jump
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.gameBorder);
             this.Name = "Block_Jump";
             this.Text = "Block Jump";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).EndInit();
+            this.gameBorder.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox block;
+        private System.Windows.Forms.Panel gameBorder;
     }
 }
 
--- Block_Jumps/Form1.cs
+++ Block_Jumps/Form1.cs
@@ -12,14 +12,40 @@ namespace Block_Jumps
 {
     public partial class Block_Jump : Form
     {
+
         public Block_Jump()
         {
             InitializeComponent();
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        public bool Collides()
+        {
+            //hindernisCheck
+            if (player.Bounds.IntersectsWith(obstacle.Bounds))    //überprüft ob player mit x(einem hindernis) kollidiert      //TODO: naming
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+
+        //TODO: in gameloopTimer implementieren
+        /*if (player.Left + player.Width - 19 > block.Left          //boden check
+                    && player.Left + player.Width<block.Left + block.Width + player.Width
+                    && player.Top + player.Height >= block.Top
+                    && player.Top<block.Top)
+            {
+                player.Top = gameBorder.Height - block.Height - player.Height;
+        }*/
+
+
     }
 }
