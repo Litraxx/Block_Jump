3f9a8c1c50dc93efbcf36efb7ef6e16e4b048707
litrax <fabien-goehl@web.de>
2018-07-24 12:13:42 +0200

    Implemented Classes from the Classdiagram Done TODOS for ImageToLevel

--- Block_Jumps/Form1.cs
+++ Block_Jumps/Form1.cs
@@ -10,28 +10,102 @@ using System.Windows.Forms;
 
 namespace Block_Jumps
 {
-	public partial class Block_Jump : Form
+	enum BoxType
 	{
-		public Block_Jump()
+		AIR,
+		BLOCK,
+		COIN,
+		SPAWN,
+		FINISH,
+		PLAYER
+	}
+
+	class Box
+	{
+		protected PictureBox picBox;
+		private BoxType type;
+		protected bool canCollide;
+
+		public Box(int x, int y, BoxType type, bool canCollide)
 		{
-			InitializeComponent();
+			picBox = new PictureBox();
+			picBox.Location = new Point(x, y);
+			this.type = type;
+			this.canCollide = canCollide;
 		}
 
-		private void Form1_Load(object sender, EventArgs e)
+		public PictureBox PicBox
+		{
+			get { return picBox; }
+			set { picBox = value; }
+		}
+
+		public BoxType Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+		public bool CanCollide
+		{
+			get { return canCollide; }
+			set { canCollide = value; }
+		}
+
+		protected bool Collides()
+		{
+			// TODO: Logik für Collides hinzufügen
+			return false;
+		}
+	}
+
+	class Player : Box
+	{
+		public Player(int x, int y)
+			: base(x, y, BoxType.PLAYER, false)
 		{
 
 		}
 
-		private void ImageToLevel(string levelFile)
+		public void Jump()
 		{
-			//Einlesen des Bildes per File
-			Image img = Image.FromFile(levelFile);
+			// TODO: Logik für Jump hinzufügen
+		}
+	}
 
+	class Level
+	{
+		private Box[,] boxes;
+		private Image levelImage;
+
+		public Level(string levelFile)
+		{
+			levelImage = Image.FromFile(levelFile);
+			boxes = new Box[levelImage.Width,levelImage.Height];
+			ImageToLevel();
+		}
+
+		public Image LevelImage
+		{
+			get { return levelImage; }
+			set { levelImage = value; }
+		}
+
+		
+
+		public Box[,] Boxes
+		{
+			get { return boxes; }
+			set { boxes = value; }
+		}
+
+		private void ImageToLevel()
+		{
 			//Kopie des Bildes zu Bitmap
-			Bitmap map = new Bitmap(img);
+			Bitmap map = new Bitmap(levelImage);
 
 			//Pixelarray
-			Color[,] pixels = new Color[map.Width, map.Height];
+			Color[,] pixels = new Color[levelImage.Width, levelImage.Height];
 
 			//Scale
 			int scale = 10;
@@ -52,26 +126,43 @@ namespace Block_Jumps
 				{
 					if (pixels[x, y].A == 0 && pixels[x, y].R == 0 && pixels[x, y].G == 0 && pixels[x, y].B == 0)
 					{
-						// TODO: Code für tranzparente Pixel hinzufügen
+						boxes[x, y] = new Box(x * scale, y * scale, BoxType.AIR, false);
 					}
 					else if (pixels[x, y].A == 255 && pixels[x, y].R < 10 && pixels[x, y].G < 10 && pixels[x, y].B < 10)
 					{
-						// TODO: Code für schwarze Pixel hinzufügen 
+						boxes[x, y] = new Box(x * scale, y * scale, BoxType.BLOCK, true);
 					}
 					else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 0 && pixels[x, y].B == 255)
 					{
-						// TODO: Code für blaue Pixel hinzufügen
+						boxes[x, y] = new Box(x * scale, y * scale, BoxType.SPAWN, true);
 					}
 					else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 255 && pixels[x, y].B == 0)
 					{
-						// TODO: Code für grüne Pixel hinzufügen 
+						boxes[x, y] = new Box(x * scale, y * scale, BoxType.FINISH, false);
 					}
 					else if (pixels[x, y].A == 255 && pixels[x, y].R == 255 && pixels[x, y].G == 255 && pixels[x, y].B == 0)
 					{
-						// TODO: Code für gelbe Pixel hinzufügen
+						boxes[x, y] = new Box(x * scale, y * scale, BoxType.COIN, false);
 					}
 				}
 			}
+		}
+	}
+
+	public partial class Block_Jump : Form
+	{
+		private Level level = new Level("testLevel.png");
+
+		Player player = new Player(10, 10);
+
+		public Block_Jump()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
