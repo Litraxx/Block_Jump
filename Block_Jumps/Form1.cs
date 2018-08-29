915d041252a77628fb9bddbc35992f1005957894
Fabien Göhl <fabien-goehl@web.de>
2018-08-29 12:26:09 +0200

    Fixed bugs PicBoxes now display on screen

--- Block_Jumps/Form1.cs
+++ Block_Jumps/Form1.cs
@@ -11,152 +11,156 @@ using System.Windows.Forms;
 namespace Block_Jumps
 {
 
-    enum BoxType
-    {
-        AIR,
-        BLOCK,
-        COIN,
-        SPAWN,
-        FINISH,
-        PLAYER
-    }
-
-    class Box
-    {
-        protected PictureBox picBox;
-        private BoxType type;
-        protected bool canCollide;
-
-        public Box(int x, int y, BoxType type, bool canCollide)//,/Color color)
-        {
-            picBox = new PictureBox();
-            picBox.Location = new Point(x, y);
-            //picBox.BackColor = color;
-            this.type = type;
-            this.canCollide = canCollide;
-        }
-
-        public PictureBox PicBox
-        {
-            get { return picBox; }
-            set { picBox = value; }
-        }
-
-        public BoxType Type
-        {
-            get { return type; }
-            set { type = value; }
-        }
-
-        public bool CanCollide
-        {
-            get { return canCollide; }
-            set { canCollide = value; }
-        }
-
-        protected bool Collides()
-        {
-            // TODO: Logik für Collides hinzufügen
-            return false;
-        }
-    }
-
-    class Player : Box
-    {
-        public Player(int x, int y)
-            : base(x, y, BoxType.PLAYER, false)
-        {
-
-        }
-
-        public void Jump()
-        {
-            // TODO: Logik für Jump hinzufügen
-        }
-    }
-
-    class Level
-    {
-        private Box[,] boxes;
-        private Image levelImage;
-
-        public Level(string levelFile)
-        {
-            levelImage = Image.FromFile(levelFile);
-            boxes = new Box[levelImage.Width, levelImage.Height];
-            ImageToLevel();
-        }
-
-        public Image LevelImage
-        {
-            get { return levelImage; }
-            set { levelImage = value; }
-        }
-
-
-
-        public Box[,] Boxes
-        {
-            get { return boxes; }
-            set { boxes = value; }
-        }
-
-        private void ImageToLevel()
-        {
-            //Kopie des Bildes zu Bitmap
-            Bitmap map = new Bitmap(levelImage);
-
-            //Pixelarray
-            Color[,] pixels = new Color[levelImage.Width, levelImage.Height];
-
-            //Scale
-            int scale = 10;
-
-            //Initialisieren des Pixelarrays
-            for (int y = 0; y < map.Height; y++)
-            {
-                for (int x = 0; x < map.Width; x++)
-                {
-                    pixels[x, y] = map.GetPixel(x, y);
-                }
-            }
-
-            //Geht jeden Pixel in Pixels durch
-            for (int y = 0; y < map.Height; y++)
-            {
-                for (int x = 0; x < map.Width; x++)
-                {
-                    //Weiß
-                    if (pixels[x, y].A == 0 && pixels[x, y].R == 0 && pixels[x, y].G == 0 && pixels[x, y].B == 0)
-                    {
-                        boxes[x, y] = new Box(x * scale, y * scale, BoxType.AIR, false);
-                    }
-                    //Schwarz
-                    else if (pixels[x, y].A == 255 && pixels[x, y].R < 10 && pixels[x, y].G < 10 && pixels[x, y].B < 10)
-                    {
-                        boxes[x, y] = new Box(x * scale, y * scale, BoxType.BLOCK, true);
-                    }
-                    //Blau
-                    else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 0 && pixels[x, y].B == 255)
-                    {
-                        boxes[x, y] = new Box(x * scale, y * scale, BoxType.SPAWN, true);
-                    }
-                    //
-                    else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 255 && pixels[x, y].B == 0)
-                    {
-                        boxes[x, y] = new Box(x * scale, y * scale, BoxType.FINISH, false);
-                    }
-                    else if (pixels[x, y].A == 255 && pixels[x, y].R == 255 && pixels[x, y].G == 255 && pixels[x, y].B == 0)
-                    {
-                        boxes[x, y] = new Box(x * scale, y * scale, BoxType.COIN, false);
-                    }
-                }
-            }
-        }
-    }
 
     public partial class Block_Jump : Form
-    { 
+    {
+
+
+        enum BoxType
+        {
+            AIR,
+            BLOCK,
+            COIN,
+            SPAWN,
+            FINISH,
+            PLAYER
+        }
+
+        class Box
+        {
+            protected PictureBox picBox;
+            private BoxType type;
+            protected bool canCollide;
+
+            public Box(int x, int y, BoxType type, bool canCollide, Color color)
+            {
+                picBox = new PictureBox();
+                picBox.Location = new Point(x, y);
+                picBox.Size = new Size(15, 15);
+                picBox.BackColor = color;
+                this.type = type;
+                this.canCollide = canCollide;
+            }
+
+            public PictureBox PicBox
+            {
+                get { return picBox; }
+                set { picBox = value; }
+            }
+
+            public BoxType Type
+            {
+                get { return type; }
+                set { type = value; }
+            }
+
+            public bool CanCollide
+            {
+                get { return canCollide; }
+                set { canCollide = value; }
+            }
+
+            protected bool Collides()
+            {
+                // TODO: Logik für Collides hinzufügen
+                return false;
+            }
+        }
+
+        class Player : Box
+        {
+            public Player(int x, int y)
+                : base(x, y, BoxType.PLAYER, false, Color.Red)
+            {
+
+            }
+
+            public void Jump()
+            {
+                // TODO: Logik für Jump hinzufügen
+            }
+        }
+
+        class Level
+        {
+            private Box[,] boxes;
+            private Image levelImage;
+
+            public Level(string levelFile)
+            {
+                levelImage = Image.FromFile(levelFile);
+                boxes = new Box[levelImage.Width, levelImage.Height];
+                ImageToLevel();
+            }
+
+            public Image LevelImage
+            {
+                get { return levelImage; }
+                set { levelImage = value; }
+            }
+
+
+
+            public Box[,] Boxes
+            {
+                get { return boxes; }
+                set { boxes = value; }
+            }
+
+            private void ImageToLevel()
+            {
+                //Kopie des Bildes zu Bitmap
+                Bitmap map = new Bitmap(levelImage);
+
+                //Pixelarray
+                Color[,] pixels = new Color[levelImage.Width, levelImage.Height];
+
+                //Scale
+                int scale = 15;
+
+                //Initialisieren des Pixelarrays
+                for (int y = 0; y < map.Height; y++)
+                {
+                    for (int x = 0; x < map.Width; x++)
+                    {
+                        pixels[x, y] = map.GetPixel(x, y);
+                    }
+                }
+
+                //Geht jeden Pixel in Pixels durch
+                for (int y = 0; y < map.Height; y++)
+                {
+                    for (int x = 0; x < map.Width; x++)
+                    {
+                        //Weiß
+                        if (pixels[x, y].A == 255 && pixels[x, y].R == 255 && pixels[x, y].G == 255 && pixels[x, y].B == 255)
+                        {
+                            boxes[x, y] = new Box(x * scale, y * scale, BoxType.AIR, false, Color.White);
+                        }
+                        //Schwarz
+                        else if (pixels[x, y].A == 255 && pixels[x, y].R < 10 && pixels[x, y].G < 10 && pixels[x, y].B < 10)
+                        {
+                            boxes[x, y] = new Box(x * scale, y * scale, BoxType.BLOCK, true, Color.Black);
+                        }
+                        //Blau
+                        else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 0 && pixels[x, y].B == 255)
+                        {
+                            boxes[x, y] = new Box(x * scale, y * scale, BoxType.SPAWN, true, Color.Blue);
+                        }
+                        //Grün
+                        else if (pixels[x, y].A == 255 && pixels[x, y].R == 0 && pixels[x, y].G == 255 && pixels[x, y].B == 0)
+                        {
+                            boxes[x, y] = new Box(x * scale, y * scale, BoxType.FINISH, false, Color.FromArgb(0, 255, 0));
+                        }
+                        //Gelb
+                        else if (pixels[x, y].A == 255 && pixels[x, y].R == 255 && pixels[x, y].G == 255 && pixels[x, y].B == 0)
+                        {
+                            boxes[x, y] = new Box(x * scale, y * scale, BoxType.COIN, false, Color.Yellow);
+                        }
+                    }
+                }
+            }
+        }
 
         public Block_Jump()
         {
@@ -171,14 +175,26 @@ namespace Block_Jumps
 
         private void Start()
         {
-            Level level = new Level("testLevel.png");
+            Level level = new Level("H:/EigeneDaten/GDS_Project/testLevel.png");
+
+            
 
             Player player = new Player(10, 10);
 
-            PictureBox box = new PictureBox();
+            //PictureBox box = new PictureBox();
+
+            foreach(Box box in level.Boxes)
+            {
+                Controls.Add(box.PicBox);
+                box.PicBox.Refresh();
+            }
             
             
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
     }
 }
